ccf7ccf76ccd6d3779a02093ad4854e2f96f8a8a
João Pedro Prado <joaopedro0128@hotmail.com>
2023-11-03 14:22:58 -0300

    feat: fixed docker related configs

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+poetry run python -m manage migrate
+
+mkdir log
+touch /app/log/access.log
+
+poetry run python -m \
+  gunicorn bnex.asgi:application -k uvicorn.workers.UvicornWorker -w 3 \
+  -b 0.0.0.0:8000 --log-level=info --access-logfile=/app/log/access.log -t 120
